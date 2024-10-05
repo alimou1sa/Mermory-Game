9bfcb09b714bb9fa134d333fc25da6f733bafdc6
Dev.Ali Mousa <ali.mou1sa@gmail.com>
2024-10-05 09:37:34 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace project_4_memory_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 97);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Timer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 69);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(59, 478);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 63);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(763, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 141);
+            this.button3.TabIndex = 17;
+            this.button3.Tag = "Girl";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(594, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 141);
+            this.button4.TabIndex = 18;
+            this.button4.Tag = "O";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(428, 91);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 141);
+            this.button5.TabIndex = 19;
+            this.button5.Tag = "?";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(254, 91);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 141);
+            this.button6.TabIndex = 20;
+            this.button6.Tag = "X";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(763, 262);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 141);
+            this.button7.TabIndex = 21;
+            this.button7.Tag = "?";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
+            this.button8.Location = new System.Drawing.Point(763, 439);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(133, 141);
+            this.button8.TabIndex = 22;
+            this.button8.Tag = "Book";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Image = ((System.Drawing.Image)(resources.GetObject("button9.Image")));
+            this.button9.Location = new System.Drawing.Point(594, 262);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 141);
+            this.button9.TabIndex = 23;
+            this.button9.Tag = "X";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button10
+            // 
+            this.button10.Image = ((System.Drawing.Image)(resources.GetObject("button10.Image")));
+            this.button10.Location = new System.Drawing.Point(428, 262);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(133, 141);
+            this.button10.TabIndex = 24;
+            this.button10.Tag = "Book";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button11
+            // 
+            this.button11.Image = ((System.Drawing.Image)(resources.GetObject("button11.Image")));
+            this.button11.Location = new System.Drawing.Point(594, 439);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(133, 141);
+            this.button11.TabIndex = 24;
+            this.button11.Tag = "Boy";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button12
+            // 
+            this.button12.Image = ((System.Drawing.Image)(resources.GetObject("button12.Image")));
+            this.button12.Location = new System.Drawing.Point(428, 439);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(133, 141);
+            this.button12.TabIndex = 25;
+            this.button12.Tag = "Girl";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button13
+            // 
+            this.button13.Image = ((System.Drawing.Image)(resources.GetObject("button13.Image")));
+            this.button13.Location = new System.Drawing.Point(255, 262);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(133, 141);
+            this.button13.TabIndex = 26;
+            this.button13.Tag = "Boy";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button14
+            // 
+            this.button14.Image = ((System.Drawing.Image)(resources.GetObject("button14.Image")));
+            this.button14.Location = new System.Drawing.Point(254, 439);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(133, 141);
+            this.button14.TabIndex = 27;
+            this.button14.Tag = "O";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(687, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 57);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Time left";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(923, 31);
+            this.menuStrip1.TabIndex = 29;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(84, 27);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // timeToolStripMenuItem
+            // 
+            this.timeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.timeToolStripMenuItem.Name = "timeToolStripMenuItem";
+            this.timeToolStripMenuItem.Size = new System.Drawing.Size(131, 28);
+            this.timeToolStripMenuItem.Text = "Time";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(112, 28);
+            this.toolStripMenuItem2.Text = "30";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(112, 28);
+            this.toolStripMenuItem3.Text = "20";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(112, 28);
+            this.toolStripMenuItem4.Text = "10";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(131, 28);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 72);
+            this.label3.TabIndex = 30;
+            this.label3.Tag = "0";
+            this.label3.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(923, 617);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Tag = "X";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using project_4_memory_Game.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_4_memory_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        byte count = 0;
+        Button btn1 = null;
+        Button btn2 = null;
+
+        byte score = 0;
+        byte timergame = 20;
+        byte timer = 3;
+
+        private bool CheckifRight(Button btn1,Button btn2)
+        {
+           
+            if(btn1.Tag==btn2.Tag)
+            {
+                score += 5;
+                label3.Text = score.ToString();
+                return true;
+            }
+
+            return false;
+        }
+
+        private void SaveChoise(Button btn)
+        {
+            if(count>2)
+            {
+                return;
+            }
+
+            if((count==0)&&(btn1==null))
+            {
+                btn1 = btn;
+               
+            }
+
+            if((count==1)&&(btn2==null))
+            {
+                btn2 = btn;
+            }
+
+
+            if ((btn1 != null) && (btn2 != null))
+            {
+                if (btn2.Name.ToString() == btn1.Name.ToString())
+                {
+                    MessageBox.Show("You cannot choose the same picture", "Choose Anther Picture");
+                    btn2 = null;
+                    return;
+                }
+
+            }
+            count++;
+           
+        }
+
+        void checktimeandscoor()
+        {
+            if(timergame==0)
+            {
+                MessageBox.Show(" Time game End","A Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (score == 30)
+            {
+                timer2.Enabled = false;
+                MessageBox.Show("you won", "final result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        void convertemage(Button btn)
+        {
+            if(btn.Tag=="X")
+            {
+                btn.Image = Resources.X;
+                return;
+            }
+            if (btn.Tag == "O")
+            {
+                btn.Image = Resources.O;
+                return;
+            }
+            if (btn.Tag == "?")
+            {
+                btn.Image = Resources.question_mark_96;
+                return;
+            }
+            if (btn.Tag == "Girl")
+            {
+                btn.Image = Resources.Girl;
+                return;
+            }
+            if (btn.Tag == "Boy")
+            {
+                btn.Image = Resources.Boy;
+                return;
+            }
+            if (btn.Tag == "Book")
+            {
+                btn.Image = Resources.Book;
+                return;
+            }
+
+        }
+
+        private void finalrisalt( Button btn)
+        { 
+       
+
+            SaveChoise(btn);
+            convertemage(btn);
+
+            if (count == 2)
+            {
+                if (CheckifRight(btn1, btn2))
+                {
+                    btn1.Visible = false;
+                    btn2.Visible = false;
+
+                    btn1 = null;
+                    btn2 = null;
+                    count = 0;
+                    checktimeandscoor();
+                }
+                else
+                {
+                        btn1.Image = Resources.Pen;
+                        btn2.Image = Resources.Pen;
+
+                        btn1 = null;
+                        btn2 = null;
+                        count = 0;
+                    
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Refresh();
+            label1.Text = timergame.ToString();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            finalrisalt((Button)sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            timer = 3;
+            timer1.Enabled = true;
+        }
+
+        void startGame()
+        {
+
+            score = 0;
+            count = 0;
+            btn1 = null;
+            btn2 = null;
+
+            if (timer == 0)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = true;
+                button3.Image = Resources.Pen;
+                button4.Image = Resources.Pen;
+                button5.Image = Resources.Pen;
+                button6.Image = Resources.Pen;
+                button7.Image = Resources.Pen;
+                button8.Image = Resources.Pen;
+                button9.Image = Resources.Pen;
+                button10.Image = Resources.Pen;
+                button11.Image = Resources.Pen;
+                button12.Image = Resources.Pen;
+                button13.Image = Resources.Pen;
+                button14.Image = Resources.Pen;
+                timer = 0;
+            }
+
+        }
+    
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+             timer--;
+            label2.Text = timer.ToString();
+            startGame();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+                timergame--;
+                label1.Text = timergame.ToString();
+
+            if (timergame == 0)
+            {
+                timer2.Enabled = false;
+                MessageBox.Show(" Time game End", "A Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            timergame = 30;
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            timergame = 20;
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            timergame = 10;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label1.Refresh();
+            label2.Text = timer.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            button6.Visible = true;
+            button9.Visible = true;
+            button5.Visible = true;
+            button7.Visible = true;
+            button4.Visible = true;
+            button3.Visible = true;
+            button13.Visible = true;
+            button10.Visible = true;
+            button14.Visible = true;
+            button12.Visible = true;
+            button11.Visible = true;
+            button8.Visible = true;
+
+            timer = 3;
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            timergame = 20;
+
+            label1.Text="20";
+            label2.Text="0";
+
+        }
+
+    
+    }
+}
